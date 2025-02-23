354ef1d37261722ac865657d3be156df38f5910b
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-23 20:40:11 -0300

    feat: asking for number of meows

--- week-01/cat.c
+++ week-01/cat.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <cs50.h>
 
-void meow(void) {
-  printf("meow\n");
+void meow(int times) {
+  for (int i = 0; i < times; i++) { // using for loop
+    printf("meow\n");
+  }
 }
 
 int main(void) {
-  for (int i = 0; i < 3; i++) { // using for loop
-    meow();
-  }
+  int n = get_int("Number of meow: ");
+  meow(n);
 }
