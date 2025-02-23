52641b59709a641f40658df685d0a92120480a02
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-23 19:11:36 -0300

    feat: just write types

---
+++ week-01/types.c
@@ -0,0 +1,8 @@
+/**
+ * bool - boolean values, like true and false
+ * int - integer numbers, like 1, 2, 3
+ * float - float numbers with decimals, like 3.14
+ * long - integer numbers but with more space, convenient for LONG numbers
+ * double - float numbers but with more precision than float
+ * char - one single character, like a letter
+ */
